543fe22301d6dc1c3e8ed7fb823b06189477d5f4
PaulPr-DHBW <blockmeisterp@gmail.com>
2026-01-29 13:51:45 +0000

    Erster Programmentwurf

---
+++ courses/coding-in-C/Lab_2/Mysolution/section1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+const char colums[8] = {'A', 'B', 'C', 'D', 'F', 'G', 'H'};
+const int ci_rowSize = 8;
+const int ci_colSize = 7;
+
+int main()
+{
+    for(int i = ci_rowSize; i >= 1; i--)
+    {
+        for(int j = ci_colSize-1; j >=0; j--)
+        {
+            printf("%c %d\t", colums[j], ci_rowSize);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
