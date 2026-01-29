1a20342981c0dbac242b07a2991e6e38658e605e
PaulPr-DHBW <blockmeisterp@gmail.com>
2026-01-29 13:58:39 +0000

    Final Version of Chessä

--- courses/coding-in-C/Lab_2/Mysolution/section1.c
+++ courses/coding-in-C/Lab_2/Mysolution/section1.c
@@ -8,7 +8,7 @@ int main()
 {
     for(int i = ci_rowSize; i >= 1; i--)
     {
-        for(int j = 0; j < ci_colSize; j--)
+        for(int j = 0; j < ci_colSize; j++)
         {
             printf("%c%d\t", colums[j], i);
         }
