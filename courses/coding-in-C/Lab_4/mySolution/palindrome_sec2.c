970b68e81829940da7484381c0a5970ee2212ce6
PaulPr-DHBW <blockmeisterp@gmail.com>
2026-02-11 11:10:22 +0000

    Finished Palindrome

--- courses/coding-in-C/Lab_4/mySolution/palindrome_sec2.c
+++ courses/coding-in-C/Lab_4/mySolution/palindrome_sec2.c
@@ -5,7 +5,7 @@ void check_palindrome(char* input_array, int length_array)
 {
     for(int i = 0; i < length_array; i++)
     {
-        if(!strcmp(input_array[i], input_array[length_array-i]))
+        if(input_array[i] != input_array[length_array-i])
         {   
             printf("Your provided Word: %s \nis not a palindrome.", input_array); 
             return;
@@ -23,7 +23,7 @@ void get_userinput(char* input_array, int size_of_input_array)
     {
         printf("Please enter a word, to check if its a palindrome: \n");
 
-    } while (fgets(input_array, size_of_input_array, stdin) != NULL);
+    } while (fgets(input_array, size_of_input_array, stdin) == NULL);
 
     input_array[strcspn(input_array, "\n")] = '\0';
 }
@@ -35,7 +35,7 @@ int main ()
 
     get_userinput(userinput_content, sizeof(userinput_content));
 
-    check_palindrome(userinput_content, strlen(userinput_content));
+    check_palindrome(userinput_content, strlen(userinput_content)-1); //-1 da terminierungs symbol
 
 
 
