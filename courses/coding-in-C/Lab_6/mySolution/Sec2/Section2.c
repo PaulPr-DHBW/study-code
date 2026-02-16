7240f8339cd976d6da8c040543f0ad265864aec5
PaulPr-DHBW <blockmeisterp@gmail.com>
2026-02-16 14:38:06 +0000

    Section 2 complete

---
+++ courses/coding-in-C/Lab_6/mySolution/Sec2/Section2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct car
+{
+    const float f_max_fuel_level;
+    float f_fuel_level;
+    char model[20];
+
+} car;
+
+
+void refuel(car *ptr_car_data, float fuel_amount)
+{
+    if(ptr_car_data->f_fuel_level + fuel_amount < ptr_car_data->f_max_fuel_level && fuel_amount > 0)
+    {
+        ptr_car_data->f_fuel_level += fuel_amount;
+    }
+    else if (fuel_amount > 0)
+    {
+        ptr_car_data->f_fuel_level = ptr_car_data->f_max_fuel_level;
+    }
+    else
+    {
+        printf("\nFuel cant be negativ!\n");
+    }
+}
+
+int main (void)
+{
+    car Mercedes = {50.0, 10.0, "AMG ONE"};
+    prtinf("Aktueller Tankinhalt: &.2f", Mercedes.f_fuel_level);
+    refuel(&Mercedes);
+    prtinf("Aktueller Tankinhalt: &.2f", Mercedes.f_fuel_level);
+
+
+    return 0;
+}
