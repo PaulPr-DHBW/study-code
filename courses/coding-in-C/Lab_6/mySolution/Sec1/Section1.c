c2bd3f6386bc3090d786097be20ec3828bc5eb92
PaulPr-DHBW <blockmeisterp@gmail.com>
2026-02-16 14:12:02 +0000

    Section1 complete

--- courses/coding-in-C/Lab_6/mySolution/Sec1/Section1.c
+++ courses/coding-in-C/Lab_6/mySolution/Sec1/Section1.c
@@ -58,12 +58,37 @@ void reading_by_fscanf(FILE* ptr_data)
     buffer = help;
     buffer[anzahl] = '\0';
 
-    printf("Data read in by fscanf:\t%s\n", buffer);
+    printf("\nData read in by fscanf:\t%s\n", buffer);
 
     free(buffer);
 }
 
+void writing_by_fputc(FILE* ptr_data)
+{
+    fputc('P', ptr_data);
+    fputc('A', ptr_data);
+    fputc('U', ptr_data);
+    fputc('L', ptr_data);
 
+}
+
+void writing_by_fputs(FILE* ptr_data)
+{
+    char* ptr_write_fputs = "Hier hat Paul geschrieben";
+
+    if(fputs(ptr_write_fputs, ptr_data) == EOF)
+        printf("\nERROR fputs\n");
+
+
+}
+
+void writing_by_fprintf(FILE* ptr_data)
+{
+    int example = 1;
+
+    fprintf(ptr_data, "Hallo %d", example);
+
+}
 
 int main()
 {
@@ -71,17 +96,28 @@ int main()
     //FILE *ptr_write_file = fopen("example.txt", "w");
 
     //Lets read in the example File
-    reading_by_fgetc(ptr_read_file);
-    rewind(ptr_read_file);
+    //reading_by_fgetc(ptr_read_file);
+    //rewind(ptr_read_file);
 
-    reading_by_fgets(ptr_read_file);
-    rewind(ptr_read_file);
+    //reading_by_fgets(ptr_read_file);
+    //rewind(ptr_read_file);
 
-    reading_by_fscanf(ptr_read_file);
-    rewind(ptr_read_file);
+    //reading_by_fscanf(ptr_read_file);
+    //rewind(ptr_read_file);
 
 
     fclose(ptr_read_file);
     //fclose(ptr_write_file);
+
+    FILE *ptr_write_file = fopen("examplewrite.txt", "w");
+
+    writing_by_fputc(ptr_write_file);
+    writing_by_fputs(ptr_write_file);
+    writing_by_fprintf(ptr_write_file);
+
+    //writing_by_fputs(ptr_write_file);
+
+
+    fclose(ptr_write_file);
     return 0;
 }
